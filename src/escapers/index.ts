3ec80f3e390d54ac813a80f5457fa8b4aa7a5a13
yUnreal <unrealwebdev@gmail.com>
2023-07-16 20:43:41 -0300

    feat(JSDoc): adding JSDoc comments to functions

--- src/escapers/index.ts
+++ src/escapers/index.ts
@@ -1,53 +1,148 @@
 import { TimestampStyles } from '../types/index';
 
-export function escapeBold<C extends string>(content: C): string {
+/**
+ * Escapes bold formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
+export function escapeBold(content: string): string {
     return content.replace(/\*\*(.*?)\*\*/g, '\\*\\*$1\\*\\*');
 }
 
+/**
+ * Escapes code block formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeCodeBlock(content: string): string {
     return content.replace(/```([^`]+)```/g, '\\`\\`\\`$1\\`\\`\\`');
 }
 
+/**
+ * Escapes inline code formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeInlineCode(content: string): string {
     return content.replace(/`([^`]+)`/g, '\\`$1\\`');
 }
 
+/**
+ * Escapes italic formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeItalic(content: string): string {
     return content.replace(/_([^_]+)_/g, '\\_$1\\_');
 }
 
+/**
+ * Escapes masked link formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeMaskedLink(content: string): string {
     // eslint-disable-next-line no-useless-escape
     return content.replace(/\[([^\[\]]+)\]\(((?:[\w-]+:)?\/\/(?:[\w-]+\.)+[a-zA-Z]{2}(?::\d+)?(?:\/[^\s]*)?)\)/g, '\\[$1\\]\\($2\\)');
 }
 
+/**
+ * Escapes number list formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeNumberedList(content: string): string {
     return content.replace(/^(\d+\.\s)/gm, '\\$1');
 }
 
+/**
+ * Escapes spoiler formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeSpoiler(content: string): string {
     return content.replace(/\|\|([\s\S]+?)\|\|/g, '\\|\\|$1\\|\\|');
 }
 
+/**
+ * Escapes strikethrough formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeStrikethrough(content: string): string {
     return content.replace(/~~([^~]+)~~/g, '\\~\\~$1\\~\\~');
 }
 
+/**
+ * Escapes underline formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeUnderline(content: string): string {
     return content.replace(/__(.*?)__/g, '\\__$1\\__');
 }
 
+/**
+ * Formats the given time as a timestamp string.
+ *
+ * @template D - The type of the time value.
+ * @template S - The style of the timestamp.
+ * @param {D} time - The time value to format.
+ * @param {S} [style] - The timestamp style.
+ * @returns {S extends TimestampStyles ? `<t:${number}:${S}>` : `<t:${number}>`} - The formatted timestamp.
+ * @since 0.0.1
+ */
 export function formatTime<D extends Date, S extends TimestampStyles = TimestampStyles>(time: D, style?: S): S extends TimestampStyles ? `<t:${number}:${S}>` : `<t:${number}>` {
     return `<t:${time.getTime() / 1000}${style ? ':' + style : ''}>` as S extends TimestampStyles ? `<t:${number}:${S}>` : `<t:${number}>`;
 }
 
+/**
+ * Escapes escape characters in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeEscape(content: string): string {
     return content.replace(/\\/g, '\\\\');
 }
+
+/**
+ * Escapes bulleted list formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeBulletedList(content: string): string {
     return content.replace(/^( *)[-+*] /gm, '$1\\$&');
 }
 
+/**
+ * Escapes headings formatting in the given content.
+ *
+ * @param {string} content - The content to format.
+ * @returns {string} - The escaped content.
+ * @since 0.0.1
+ */
 export function escapeHeadings(content: string): string {
     return content.replace(/(#{1,6}) /gm, '\\$1 ');
 }
--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -1,65 +1,179 @@
 import { Snowflake } from '../types/index';
 
+/**
+ * Formats the content into a block quote.
+ * 
+ * @param {C} content - The content to format.
+ * @returns {`>>> ${C}`} The formatted content.
+ * @since 0.0.1
+ */
 export function blockQuote<C extends string>(content: C): `>>> ${C}` {
     return `>>> ${content}`;
 }
 
+/**
+ * Formats the content into a bold text.
+ * 
+ * @param {C} content - The content to format.
+ * @returns {`**${C}**`} - The formatted content.
+ * @since 0.0.1
+ */
 export function bold<C extends string>(content: C): `**${C}**` {
     return `**${content}**`;
 }
 
+/**
+ * Formats the channel Id for a direct message channel.
+ * 
+ * @param {C} channelId - The content to format.
+ * @returns {`https://discord.com/channels/@me/${C}`} - The formatted content.
+ * @since 0.0.1
+ */
 export function channelURL<C extends Snowflake>(channelId: C): `https://discord.com/channels/@me/${C}` {
     return `https://discord.com/channels/@me/${channelId}`;
 }
 
+/**
+ * Formats a channel Id into a channel mention.
+ * 
+ * @param {C} channelId - The channel Id to use.
+ * @returns {`<#${C}>`} - The formatted content.
+ * @since 0.0.1
+ */
 export function channelMention<C extends Snowflake>(channelId: C): `<#${C}>` {
     return `<#${channelId}>`;
 }
 
+/**
+ * Formats the content and tha language into a code block
+ * 
+ * @param {C} content - The content to format.
+ * @param {L} language - The language to add (e.g. ts)
+ * @returns {L extends string ? `\`\`\`${L}\n${C}\n\`\`\`` : `\`\`\`\n${C}\n\`\`\``} - The formatted content.
+ * @since 0.0.1
+ */
 export function codeBlock<C extends string, L extends string>(content: C, language?: L): L extends string ? `\`\`\`${L}\n${C}\n\`\`\`` : `\`\`\`\n${C}\n\`\`\`` {
     return (language ? `\`\`\`${language}\n${content}\n\`\`\`` : `\`\`\`\n${content}\n\`\`\``) as L extends string ? `\`\`\`${L}\n${C}\n\`\`\`` : `\`\`\`\n${C}\n\`\`\``;
 }
 
+/**
+ * Formats the emojiId into a emoji format.
+ * 
+ * @param {C} emojiId - The channel Id to use.
+ * @param {A} animated - Whether the emoji is animated.
+ * @returns {A extends true ? `<a:_:${C}>` : `<:_:${C}>`}
+ */
 export function formatEmoji<C extends Snowflake, A extends boolean = false>(emojiId: C, animated?: A): A extends true ? `<a:_:${C}>` : `<:_:${C}>` {
     return (animated ? `<a:_:${emojiId}` : `<:_:${emojiId}>`) as A extends true ? `<a:_:${C}>` : `<:_:${C}>`;
 }
 
+/**
+ * Formats the url into a hiden url.
+ * 
+ * @param {C} url - The url to use.
+ * @returns {`<${C}>`} - The formatted content.
+ * @since 0.0.1
+ */
 export function hideLinkEmbed<C extends string>(url: C): `<${C}>` {
     return `<${url}>`;
 }
 
+/**
+ * Formats the content and url into a hyperlink
+ * 
+ * @param {C} content - The content to use.
+ * @param {U} url - The url to use.
+ * @returns {`[${C}](${U})`} - The formatted content.
+ * @since 0.0.1
+ */
 export function hyperlink<C extends string, U extends string>(content: C, url: U): `[${C}](${U})` {
     return `[${content}](${url})`;
 }
 
+/**
+ * Wraps the content inside backticks which formats it as inline code.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`\`${C}\``} - The formatted content.
+ * @since 0.0.1
+ */
 export function inlineCode<C extends string>(content: C): `\`${C}\`` {
     return `\`${content}\``;
 }
 
+/**
+ * Formats the content into a italic text.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`_${C}_`} - The formatted content.
+ * @since 0.0.1
+ */
 export function italic<C extends string>(content: C): `_${C}_` {
     return `_${content}_`;
 }
 
+/**
+ * Formats the content into a quote.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`> ${C}`} - The formatted content.
+ * @since 0.0.1
+ */
 export function quote<C extends string>(content: C): `> ${C}` {
     return `> ${content}`;
 }
 
+/**
+ * Formats the role Id into a role mention.
+ * 
+ * @param {I} roleId - The role Id.
+ * @returns {`<@&${I}>`}
+ * @since 0.0.1
+ */
 export function roleMention<I extends Snowflake>(roleId: I): `<@&${I}>` {
     return `<@&${roleId}>`;
 }
 
+/**
+ * Formats the user Id into a user mention.
+ * 
+ * @param {I} userId - The user Id.
+ * @returns {`<@${I}>`} - The formatted content.
+ * @since 0.0.1
+ */
 export function userMention<I extends Snowflake>(userId: I): `<@${I}>` {
     return `<@${userId}>`;
 }
 
+/**
+ * Formats the content into a spoiler text.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`||${C}||`} - The formatted content.
+ * @since 0.0.1
+ */
 export function spoiler<C extends string>(content: C): `||${C}||` {
     return `||${content}||`;
 }
 
+/**
+ * Formats the content into a strikethrough text.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`~~${C}~~`} - The formatted content.
+ * @since 0.0.1
+ */
 export function strikethrough<C extends string>(content: C): `~~${C}~~` {
     return `~~${content}~~`;
 }
 
+/**
+ * Formats the content into underlined text.
+ * 
+ * @param {C} content - The content to use.
+ * @returns {`__${C}__`} - The formatted content.
+ * @since 0.0.1
+ */
 export function underscore<C extends string>(content: C): `__${C}__` {
     return `__${content}__`;
 }
